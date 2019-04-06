164be770c2fed1084243c9c67883f89fc88de3a1
shizidada <shizidada@gmail.com>
2019-04-07 00:09:01 +0800

    update some ～

--- react-new/src/components/app/index.js
+++ react-new/src/components/app/index.js
@@ -7,9 +7,8 @@ class App extends Component {
     render() {
         return (
             <div className={style.app}>
-                <span>React New</span>
-                <span>React New</span>
-                <span>React New</span>
+                <span>React Demo New~</span>
+                <h2>React Learn</h2>
             </div>
         )
     }
