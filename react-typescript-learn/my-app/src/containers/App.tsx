ad43fca43b545821a4830decedd2a10e1f5f2410
JiangJing <jiangjing@wshifu.com>
2019-07-15 17:41:49 +0800

    update: my-app react-placeholder

--- react-typescript-learn/my-app/src/containers/App.tsx
+++ react-typescript-learn/my-app/src/containers/App.tsx
@@ -1,25 +1,57 @@
 import * as React from "react";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
+import ReactPlaceholder from "react-placeholder";
+import "react-placeholder/lib/reactPlaceholder.css";
 import { increment, decrement } from "../store/counter/actions";
 import { CounterAction } from "../store/counter/types";
 import { AppState } from "../store";
 
+import "./App.scss";
+
 interface IProps {}
 
-interface IState {}
+interface IState {
+  ready: boolean;
+}
 
 class App extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
+    this.state = {
+      ready: false,
+    };
   }
 
   componentDidMount() {
-    console.log(this.props)
+    console.log(this.props);
+    setTimeout(() => {
+      this.setState({
+        ready: true,
+      });
+    }, 3000);
+  }
+
+  setShowButton = (enter: boolean) => {
+    this.setState({
+    })
   }
 
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+          <ReactPlaceholder
+            rows={70}
+            ready={this.state.ready}
+            showLoadingAnimation={true}
+            delay={1000}
+            style={{ width: 152 }}
+            color="#eff9fa"
+          >
+            <span>ReactPlaceholder</span>
+          </ReactPlaceholder>
+      </div>
+    );
   }
 }
 
