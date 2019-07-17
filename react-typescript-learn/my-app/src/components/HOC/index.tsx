7f4fa3791c76e51e22285675c6fb18d0c691ef95
JiangJing <jiangjing@wshifu.com>
2019-07-17 16:37:26 +0800

    add: typescript hoc

---
+++ react-typescript-learn/my-app/src/components/HOC/index.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+//https://blog.csdn.net/weixin_34221775/article/details/91364192
+
+// 声明一个 props interface，将会被添加到被包裹的组件上。
+interface WithLoadingProps {
+  loading: boolean;
+}
+
+// P表示传递到HOC的组件的props。
+// React.ComponentType<P> 是 React.FunctionComponent<P> | React.ClassComponent<P>的别名，
+// 表示传递到HOC的组件可以是类组件或者是函数组件。
+const withLoading = <P extends object>(Component: React.ComponentType<P>) =>
+  class WithLoading extends React.Component<P & WithLoadingProps> {
+
+    displayName = `HOC(${this.getDisplayName(Component)})`;
+    private getDisplayName(WrapperComponent: React.ComponentType<P>): string {
+      return WrapperComponent.displayName || WrapperComponent.name || "Component";
+    }
+
+    componentDidMount() {
+      console.log("WithLoading props", this.props)
+    }
+
+    render() {
+      const { loading, ...props } = this.props;
+      return loading ? <div>Loading</div> : <Component {...(props as P)} />;
+    }
+  };
+
+interface HocProps {
+  name: string;
+  loading: boolean;
+}
+interface HocState {}
+
+@withLoading
+export default class HOC extends React.Component<HocProps, HocState> {
+
+  componentDidMount() {
+
+  }
+  render() {
+    return <div>{this.props.name}</div>;
+  }
+}
--- react-typescript-learn/my-app/src/pages/home/index.tsx
+++ react-typescript-learn/my-app/src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import Counter from "../../containers/Counter";
 import HookItem from "../../components/HookItem";
+import HOC from "../../components/HOC";
 
 export default class HomePage extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class HomePage extends React.Component {
       <div>
         <Counter />
         <HookItem />
+        <HOC name="HOC NAME" loading={true}/>
       </div>
     );
   }
