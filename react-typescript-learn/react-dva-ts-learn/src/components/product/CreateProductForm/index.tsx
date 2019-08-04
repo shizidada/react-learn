f04b524994c93c7bd2de0c608f73322aac9d50f4
shizidada <shizidada@gmail.com>
2019-08-04 23:02:35 +0800

    update: create product form

--- react-typescript-learn/react-dva-ts-learn/src/components/product/CreateProductForm/index.tsx
+++ react-typescript-learn/react-dva-ts-learn/src/components/product/CreateProductForm/index.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Form, Input, Button, Upload, Icon } from 'antd';
+import { Form, Input, InputNumber, Button, Upload, Icon } from 'antd';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
+
+const { TextArea } = Input;
 
 interface CreateProductFormProps {
   form: WrappedFormUtils;
@@ -11,12 +13,29 @@ class CreateProductForm extends Component<CreateProductFormProps, {}> {
 
   }
 
-  private normFile = (event: any[]) => {
+  private uploadImageEvent = (event: any) => {
     console.log('Upload event:', event);
-    // if (Array.isArray(event)) {
-    //   return event;
-    // }
-    // return event && event.fileList;
+    if (event.file && event.file.response) {
+      if (event.file.response.data) {
+        const { productImage } = event.file.response.data;
+        if (productImage.status) {
+          console.log('上传成功 ', productImage);
+          return productImage.path;
+        }
+        return '上传失败'
+      }
+      return '上传失败'
+    }
+    return '上传失败'
+  };
+
+  private handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   };
 
 
@@ -33,30 +52,99 @@ class CreateProductForm extends Component<CreateProductFormProps, {}> {
     const { getFieldDecorator } = this.props.form;
     return (
       <div>
-        <Form layout="horizontal">
+        <Form layout="horizontal" onSubmit={this.handleSubmit} >
           <Form.Item label="商品名称" {...formItemLayout}>
-            <Input placeholder="请输入商品名称" />
+            {
+              getFieldDecorator('productName', {
+                rules: [{ required: true, message: '请输入商品名称!' }],
+              })(
+                <Input
+                  placeholder="请输入商品名称"
+                  name="productName" />,
+              )
+            }
           </Form.Item>
-          <Form.Item label="商品名称" {...formItemLayout}>
-            <Input placeholder="请输入商品名称" />
+          <Form.Item label="商品品牌" {...formItemLayout}>
+            {
+              getFieldDecorator('productBrand', {
+                rules: [{ required: true, message: '请输入商品品牌!' }],
+              })(
+                <Input
+                  placeholder="请输入商品品牌"
+                  name="productBrand" />,
+              )
+            }
           </Form.Item>
-          <Form.Item label="上传图片" extra="请上传商品图片" {...formItemLayout}>
-            {getFieldDecorator('upload', {
-              valuePropName: 'fileList',
-              getValueFromEvent: this.normFile,
+          <Form.Item label="商品分类" {...formItemLayout}>
+            {
+              getFieldDecorator('productCategory', {
+                rules: [{ required: true, message: '请输入商品分类!' }],
+              })(
+                <Input
+                  placeholder="请输入商品分类"
+                  name="productCategory" />,
+              )
+            }
+          </Form.Item>
+          <Form.Item label="商品库存" {...formItemLayout}>
+            {
+              getFieldDecorator('productStock', {
+                rules: [{ required: true, message: '请输入商品库存!' }],
+              })(
+                <InputNumber
+                  placeholder="请输入商品库存"
+                  name="productStock"
+                  min={0}
+                  max={10000}
+                  step={1} />,
+              )
+            }
+          </Form.Item>
+          <Form.Item label="商品价格" {...formItemLayout}>
+            {
+              getFieldDecorator('productPrice', {
+                rules: [{ required: true, message: '请输入商品价格!' }],
+              })(
+                <InputNumber
+                  placeholder="请输入商品价格"
+                  name="productPrice"
+                  min={0}
+                  max={10000}
+                  step={1} />,
+              )
+            }
+          </Form.Item>
+          <Form.Item label="商品描述" {...formItemLayout}>
+            {
+              getFieldDecorator('productDesc', {
+                rules: [{ required: true, message: '请输入商品价格!' }],
+              })(
+                <TextArea
+                  placeholder="请输入商品描述"
+                  rows={4}
+                  name="productDesc" />,
+              )
+            }
+          </Form.Item>
+          <Form.Item label="商品图片"
+            {...formItemLayout}>
+            {getFieldDecorator('productImage', {
+              // valuePropName: 'fileList',
+              rules: [{ required: true, message: '请上传商品图片!' }],
+              getValueFromEvent: this.uploadImageEvent,
             })(
-              <Upload name="logo" action="/upload.do" listType="picture">
+              <Upload
+                action=""
+                name="productImage"
+                listType="picture">
                 <Button>
                   <Icon type="upload" /> 上传图片
                 </Button>
               </Upload>,
             )}
           </Form.Item>
-          <Form.Item label="商品图片" {...formItemLayout}>
-            <span>商品图片</span>
-          </Form.Item>
           <Form.Item {...buttonItemLayout}>
-            <Button type="primary">提交</Button>
+            <Button type="primary" htmlType="submit" >提交</Button>
           </Form.Item>
         </Form>
       </div>
