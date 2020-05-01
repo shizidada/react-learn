e429fd2132016088bc5b0a0225f24926704b8d31
shizidada <shizidada@gmail.com>
2020-05-01 22:31:33 +0800

    update: lodash omit

--- react-typescript-learn/react-dva-ts-learn/src/pages/IndexPage/loadsh.test.ts
+++ react-typescript-learn/react-dva-ts-learn/src/pages/IndexPage/loadsh.test.ts
@@ -1,22 +1,22 @@
-import { includes } from 'lodash';
+import { includes, omit } from 'lodash';
 
 const s1: any = {
   installRequire: {
+    needDemolish: 'needDemolish',
     noCeiling: 'noCeiling',
   },
 };
 
-const s2: string[] = ['isCeiling', 'noCeiling', 'sunRoom'];
+// const s2: string[] = ['isCeiling', 'noCeiling', 'sunRoom'];
+// s2.map((item: string) => {
+//   if (includes(s1.installRequire, item)) {
+//     delete s1.installRequire[item];
+//   } else {
+//     s1.installRequire[item] = item;
+//   }
+// });
 
-s2.map((item: string) => {
-  if (includes(s1.installRequire, item)) {
-    delete s1.installRequire[item];
-  } else {
-    s1.installRequire[item] = item;
-  }
-});
-
-console.log(s1);
+// console.log(s1);
 
 const s3 = {
   isCeiling: [],
@@ -26,4 +26,7 @@ const s3 = {
 
 const { noCeiling: current, ...rest } = s3;
 
-console.log(current, Object.keys(rest));
+s1.installRequire = omit(s1.installRequire, Object.keys(rest))
+console.log(s1);
+
+// console.log(current, Object.keys(rest));
