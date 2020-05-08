2320e516409933f78cc4b898fdd8ce68b1ab3f4b
江景 <taohua@taohua.local>
2020-05-08 23:39:34 +0800

    update: react-virtualized test

--- react-typescript-learn/react-dva-ts-learn/src/pages/IndexPage/index.tsx
+++ react-typescript-learn/react-dva-ts-learn/src/pages/IndexPage/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import './loadsh.test'
+import { List, ListRowProps } from 'react-virtualized';
 
+import './loadsh.test';
 import './index.less';
 
 const IndexPage: React.FunctionComponent = () => {
-  return <div className="index-page-contaienr"></div>;
+  const renderItem = ({ index, isScrolling, key, style }: ListRowProps) => {
+    return (
+      <span key={key}>
+        {key}IndexPage Virtualized List <br />
+      </span>
+    );
+  };
+
+  return (
+    <div className="index-page-contaienr">
+      <List height={800} rowHeight={80} rowCount={1000} width={300} rowRenderer={renderItem} />
+    </div>
+  );
 };
 
 export default IndexPage;
